8500f2745b0bfb0547eed33ce52dba8e7eb2ffff
reekol <cupuyc@gmail.com>
2019-08-18 00:24:36 +0300

    Updated boilerplate

--- backend.php
+++ backend.php
@@ -1,13 +1,19 @@
 <?php
 
 header('Content-Type: application/json');
-
 $json = file_get_contents('php://input');
 $data = json_decode($json);
-$response = 'say something';
+
+$class      = $data->instructions[0];
+$processor  = $data->instructions[1];
+$payload    = $data->instructions[2];
+$response   = implode(' ',$data->instructions);
 
 echo json_encode([
-        'text'  => $response,
-        'input' => $data,
-        'clear' => true
+        'text'      => $response,
+        'input'     => $data,
+        'clear'     => true,
+        'class'     => $class,
+        'processor' => $processor,
+        'payload'   => $payload,
     ]);
